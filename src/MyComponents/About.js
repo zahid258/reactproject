a8939672b976a8a45983d1d5796280714630afda
zahid258 <zahid2581@gmail.com>
2021-09-06 15:37:44 +0500

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component zahid again zahid
+            This is an about component zahid again zahid2
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
         </div>
     )
